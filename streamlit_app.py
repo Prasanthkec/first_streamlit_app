e3c950bf4b31a7890bf667173e7b33d752b8f651
Prasanthkec <80454687+Prasanthkec@users.noreply.github.com>
2023-09-09 14:40:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruitload list contains")
-streamlit.text(my_data_rows)
+streamlit.dataframe(my_data_rows)
 
 
 streamlit.title('My parents first Healthy Diner')
